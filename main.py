87bb24b3e3b6b08ba779f619d25c992619fdca92
lewis <lewisjcummins@hotmail.co.uk>
2024-12-07 17:34:06 +0000

    Add sound effect

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from gpiozero import Button, LED
 from time import sleep
+from playsound import playsound
 
 button = Button(2)
 led = LED(4)
 
 led.toggle()
 
-while True:
-    if button.is_pressed:
-        print('Pressed')
-    else:
-        print('Released')
-    sleep(1)
+try:
+    while True:
+        if button.is_pressed:
+            led.off()
+            playsound('./sound.mp3')
+            led.on()
+        else:
+            sleep(0.2)
+except:
+    pass
